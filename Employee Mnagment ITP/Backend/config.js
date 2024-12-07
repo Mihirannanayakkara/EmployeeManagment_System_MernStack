63030d1c41c8bb49bedba38cf260163627baf650
Mihiran Nanayakkara <145740341+Mihirannanayakkara@users.noreply.github.com>
2024-12-07 19:10:03 +0530

    Update config.js

--- Employee Mnagment ITP/Backend/config.js
+++ Employee Mnagment ITP/Backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb+srv://MIHIR4N:Thamindu12345@employeemanagmnet.wvqcmks.mongodb.net/Employee-collection?retryWrites=true&w=majority&appName=EmployeeManagmnet'
+export const mongoDBURL = 'mongodb+srv://MIHIR4N:password@employeemanagmnet.wvqcmks.mongodb.net/Employee-collection?retryWrites=true&w=majority&appName=EmployeeManagmnet'
